09864b489b5c01cd38701c20b174d1b67ff00620
Min Cho <minsangcho19@gmail.com>
2021-12-20 09:17:45 -0800

    done but has error?

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+  const list = document.getElementById("tasks");
+  const input = document.getElementById("new-task-description");
+  const deleteForm = document.getElementById("delete-task-form")
+  const createForm = document.getElementById("create-task-form")
+  
+  createForm.addEventListener("submit", function(e) {
+    addToDo(input.value);
+    e.preventDefault();  //when the button is submitted, i need to invoke the preventDefault Method
+  })
+
+  function addToDo(toDo){
+    const element = document.createElement('li');
+    element.textContent = toDo;
+    list.append(element);
+  }
 });
+
+  deleteForm.addEventListener("submit", function(e) {
+  // e.preventDefault()
+  // e.stopPropagation()
+  //e.preventDefault();
+  deleteList();
+  })
+
+function deleteList(toDo){
+    const list = document.getElementById("tasks");
+    list.remove();
+    list = document.createElement('ul');
+    list.id = "tasks";
+    document.div.h2.append(list);
+}
